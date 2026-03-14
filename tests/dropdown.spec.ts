3dc29a23183add75138c0ee87388c036a8371ff9
diuhien1999 <diuhien04061999@gmail.com>
2026-03-14 14:20:51 +0700

    add function to study

---
+++ tests/dropdown.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+
+// Open browser
+
+// Navigate to https://the-internet.herokuapp.com/dropdown
+
+// Select "option 1"
+
+// Validate "option 1" is selected
+
+test('Dropdown', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/dropdown');
+    
+    await page.locator(`#dropdown`).selectOption({ label: 'Option 1' });
+    // await expect(page.locator(`#dropdown`)).toHaveValue('1');
+    await expect(page.locator(`#dropdown > option:checked`)).toHaveText('Option 1');
+});
+
+test('Dropdown with multiple options', async ({ page }) => {
+    await page.goto('https://output.jsbin.com/osebed/2');
+
+    await page.locator('#fruits').selectOption(['apple', 'banana']);
+    await expect(page.locator('#fruits > option:checked')).toHaveText(['Banana', 'Apple' ]);
+
+    await page.locator('#fruits').selectOption([]);
+    await expect(page.locator('#fruits > option:checked')).toHaveText([]);
+});
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,3 +8,47 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
   await page.getByRole('button', { name: ' Login' }).click();
 });
+
+test('locator with get label', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('tomsmith');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+});
+
+test('locator with get CSS & XPath', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+    await page.locator('#username').click();
+    await page.locator('#username').fill('tomsmith');
+    await page.locator('#password').click();
+    await page.locator('#password').fill('SuperSecretPassword!');
+    await page.locator('//button[@type="submit"]').click();
+});
+
+test('locator with get ID (CSS)', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+    await page.locator('#username').click();
+    await page.locator('#username').fill('tomsmith');
+    await page.locator('#password').click();
+    await page.locator('#password').fill('SuperSecretPassword!');
+    await page.locator('#login').click();
+});
+
+test('locator ', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+    await page.locator('input[name="username"]').fill('tomsmith');
+    await page.locator('input[name="password"]').fill('SuperSecretPassword!');
+    await page.locator('button[type="submit"]').click();
+});
+
+test('locator by xpath', async ({ page }) => {
+    //relative path
+    await page.goto('https://the-internet.herokuapp.com/login');
+    await page.locator('//input[@name="username"]').fill('tomsmith');
+    await page.locator('//input[@name="password"]').fill('SuperSecretPassword!');
+    await page.locator('//button[@type="submit"]').click();
+});
+
+
